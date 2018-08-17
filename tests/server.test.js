a54ecc9bacbf352373afeb9d45f8055ad7b83e92
Shaun Haddrill <shaun@custom.website>
2018-08-17 13:32:48 +0000

    Modified the server.js test to not specify the default port

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,5 +1,5 @@
 const server = require('../library/server')
 
 test('Starts express server and returns true on success', () => {
-  expect(server(80)).toBeTruthy()
+  expect(server()).toBeTruthy()
 })
